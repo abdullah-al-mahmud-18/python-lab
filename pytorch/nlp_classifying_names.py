6dbbe9bb53b0879ede51d733304048b25a297de5
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-08-31 20:16:07 +0600

    chore: pytorch nlp classifyer unfinished

--- pytorch/nlp_classifying_names.py
+++ pytorch/nlp_classifying_names.py
@@ -1,6 +1,15 @@
+from typing import Any
+
 import torch
 import string
 import unicodedata
+
+from io import open
+import os
+import time
+import glob
+
+from torch.utils.data import Dataset
 
 PRINT = True
 
@@ -40,3 +49,32 @@ if PRINT:
     print(f"letter c to index: {letter_to_index("c")}")
     print (f"The letter 'a' becomes {line_to_tensor('a')}") #notice that the first position in the tensor = 1
     print (f"The name 'Ahn' becomes {line_to_tensor('Ahn')}") #notice 'A' sets the 27th index to 1
+
+
+class NamesDataset(Dataset):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.load_time = time.localtime
+        labels_set = set()
+
+        self.data = []
+        self.data_tensors = []
+        self.labels = []
+        self.labels_tensors = []
+
+        text_files = glob.glob(os.path.join(data_dir, "*.txt"))
+        for file_name in text_files:
+            label = os.path.splitext(os.path.basename(file_name))[0]
+            labels_set.add(label)
+            lines = open(file_name, encoding='utf-8').read().strip().split('\n')
+
+            for name in lines:
+                self.data.append(name)
+                self.data_tensors.append(line_to_tensor(name))
+                self.labels.append(label)
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
