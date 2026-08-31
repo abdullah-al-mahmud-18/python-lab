8281782e9ae03b7180182264dc94de83daee282c
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-08-31 17:37:24 +0600

    feat: add more os module usage examples

--- modules/os_module.py
+++ modules/os_module.py
@@ -30,3 +30,26 @@ os.environ["KEY"] = "12345"
 key_env = os.getenv("KEY")
 print(key_env)
 
+dirs = os.listdir(cwd) # list everything (files and folders) in given folder
+print(dirs)
+
+# walk through all folders, subfolders and files
+# for root, dir, files in os.walk(cwd):
+#     for file in files:
+#         full_path = os.path.join(root, file)
+#         print(full_path)
+
+main_file_path = os.path.join(cwd, "main.py")
+
+print(main_file_path)
+
+main_file = os.path.basename(main_file_path)
+print(main_file)
+
+main_file_dir_name = os.path.dirname(main_file_path)
+print(main_file_dir_name)
+
+print(os.path.splitext(main_file))
+
+print(os.path.abspath(main_file))
+
